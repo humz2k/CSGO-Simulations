1c81a7455bf90f3a2a07b5df25904122dc783f55
humz <hqureshi@uchicago.edu>
2022-05-08 21:49:36 -0500

    thing

--- better_simulation.py
+++ better_simulation.py
@@ -1,24 +1,31 @@
-import csgo_sims
+# %% markdown
+## Simulating the Challenger Stage of PGL Antwerp
+# %% codecell
+import csgo_sims #load simulation
 from random import choice,randint,uniform
-
 csgo_sims.table.reset()
-
+teams = csgo_sims.teams #alias teams
+# %% markdown
+#### Load curated list of tier 1/tier 2 teams
+# %% codecell
 tiers = {}
 with open("tiers.txt","r") as f:
     for rawtier in f.read().split("#")[1:]:
         name = rawtier.split("\n")[0].lower()
         teams = rawtier.split("\n")[1:-1]
         tiers[name] = teams
-
-teams = csgo_sims.teams
-
+# %% markdown
+#### Function to find recent matches
+# %% codecell
 def find_recents(team):
     out = []
     for match in csgo_sims.recentmatches:
         if match["winner"] == team.name or match["loser"] == team.name:
             out.append(match)
     return out
-
+# %% markdown
+#### We use the list of curated teams to figure out the actual "tier" of a team by checking how many games a team has won against tier 1/tier 2/tier 3 teams
+# %% codecell
 def find_tier(team,tier_weights):
     tier = 0
     data = {"tier1":{"wins":0,"losses":0},"tier2":{"wins":0,"losses":0},"tier3":{"wins":0,"losses":0}}
@@ -44,7 +51,9 @@ def find_tier(team,tier_weights):
                 data["tier3"]["losses"] += 1
                 tier -= tier_weights["tier3"]["loss"]
     return tier,data
-
+# %% markdown
+#### We then use this, and data that describes the recent form of a team, to calculate a normalized "form" value
+# %% codecell
 def norm_form(team,
             tier_min = 12,tier_max=40,
             sig_winstreak=5,winstreak_weight=4,last3monthsweight=1,
@@ -54,7 +63,9 @@ def norm_form(team,
     relative_tier = (find_tier(team,tier_weights)[0]+tier_min)/tier_max
     normalized_form = recent_form*relative_tier
     return normalized_form
-
+# %% markdown
+#### We calculate the "on paper" win/loss probability of two teams using the hltv rating and the seed of the teams
+# %% codecell
 def on_paper(team1,team2,hltv_weight=0.8,seed_weight=0.2):
     total_hltv = team1.stats["hltv"] + team2.stats["hltv"]
     team1_hltv = team1.stats["hltv"]/total_hltv
@@ -66,7 +77,9 @@ def on_paper(team1,team2,hltv_weight=0.8,seed_weight=0.2):
     team1_total = (team1_seed * seed_weight + team1_hltv * hltv_weight)/(hltv_weight + seed_weight)
     team2_total = (team2_seed * seed_weight + team2_hltv * hltv_weight)/(hltv_weight + seed_weight)
     return team1_total/(team1_total + team2_total)
-
+# %% markdown
+#### Find any historical matches
+# %% codecell
 def get_history(team1,team2):
     wins = 0
     losses = 0
@@ -79,13 +92,17 @@ def get_history(team1,team2):
         return 0.5
     else:
         return wins/(wins+losses)
-
+# %% markdown
+#### Calculate the "variability" of a team (a measure of the experience levels of the team)
+# %% codecell
 def variabililty(team,worldrankingmax=53,weekstop30max=211,major_effect=2,optimal_age=26):
     rank = (worldrankingmax - team.stats["worldranking"])/worldrankingmax
     weekstop30 = (team.stats["weekstop30"]+major_effect)/(weekstop30max+major_effect)
     age = 1-(abs(team.stats["averageplayerage"]-optimal_age)/optimal_age)
     return (1 - (rank*weekstop30*age))**2
-
+# %% markdown
+#### Calculate the win probability as a combination of the above
+# %% codecell
 def rate(team1,team2,history_weight=0.1,form_weight=3,var_weight=1,upset_weight=0.2):
     team1rate = on_paper(team1,team2)
     team2rate = 1-team1rate
@@ -125,7 +142,9 @@ def rate(team1,team2,history_weight=0.1,form_weight=3,var_weight=1,upset_weight=
     team1rate /= total
     team2rate /= total
     return team1rate,team2rate
-
+# %% markdown
+#### Function that uses the probability from the above function
+# %% codecell
 def simfunc(team1,team2):
     team1win,team2win = rate(team1,team2)
     bo3 = csgo_sims.isbo3(team1,team2)
@@ -154,5 +173,7 @@ def simfunc(team1,team2):
         return team1,csgo_sims.GameStats(team1totalrounds-team2totalrounds,(team1games,team2games))
     else:
         return team2,csgo_sims.GameStats(team2totalrounds-team1totalrounds,(team2games,team1games))
-
+# %% markdown
+#### Run simulation
+# %% codecell
 results = csgo_sims.table.do_sims(simfunc,nsims=1000,file="results.txt")
